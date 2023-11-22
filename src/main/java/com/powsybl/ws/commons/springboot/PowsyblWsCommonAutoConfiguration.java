8ff3213dcaca1f098d1474e49611f54a221d0d78
Tristan <135599584+Tristan-WorkGH@users.noreply.github.com>
2023-11-22 11:13:59 +0100

    Fix lombok annotations for Javadoc remove argument JRE8+ (#42)
    Signed-off-by: Tristan Chuine <tristan.chuine_externe@rte-france.com>

--- src/main/java/com/powsybl/ws/commons/springboot/PowsyblWsCommonAutoConfiguration.java
+++ src/main/java/com/powsybl/ws/commons/springboot/PowsyblWsCommonAutoConfiguration.java
@@ -1,9 +1,14 @@
+/**
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.ws.commons.springboot;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.catalina.startup.Tomcat;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -15,7 +20,7 @@ import org.springframework.context.annotation.Bean;
 @Slf4j
 @AutoConfiguration
 @EnableConfigurationProperties({ PowsyblWsCommonProperties.class })
-@AllArgsConstructor(onConstructor_ = {@Autowired})
+@AllArgsConstructor()
 public class PowsyblWsCommonAutoConfiguration {
     private final PowsyblWsCommonProperties properties;
 
--- src/main/java/com/powsybl/ws/commons/springboot/TomcatCustomization.java
+++ src/main/java/com/powsybl/ws/commons/springboot/TomcatCustomization.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.ws.commons.springboot;
 
 import com.powsybl.ws.commons.springboot.PowsyblWsCommonProperties.TomcatPowsyblProperties;
@@ -5,13 +11,12 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.catalina.connector.Connector;
 import org.apache.tomcat.util.buf.EncodedSolidusHandling;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-@AllArgsConstructor(onConstructor_ = {@Autowired})
+@AllArgsConstructor()
 public class TomcatCustomization implements TomcatConnectorCustomizer {
     private final TomcatPowsyblProperties properties;
 

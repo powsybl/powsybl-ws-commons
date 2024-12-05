86386f3c71229b2c3af085924e52931820d21094
jamal-khey <myjamal89@gmail.com>
2024-12-05 13:06:37 +0100

    add license and author
    Signed-off-by: jamal-khey <myjamal89@gmail.com>

--- src/main/java/com/powsybl/ws/commons/computation/utils/ComputationResultUtils.java
+++ src/main/java/com/powsybl/ws/commons/computation/utils/ComputationResultUtils.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.ws.commons.computation.utils;
 
 import com.powsybl.iidm.network.*;
@@ -12,6 +18,9 @@ import java.util.stream.Collectors;
 import static com.powsybl.iidm.network.IdentifiableType.BUSBAR_SECTION;
 import static com.powsybl.security.ViolationLocation.Type.NODE_BREAKER;
 
+/**
+ * @author Jamal KHEYYAD <jamal.kheyyad at rte-international.com>
+ */
 public final class ComputationResultUtils {
 
     private ComputationResultUtils() {
--- src/test/java/com/powsybl/ws/commons/computation/ComputationUtilTest.java
+++ src/test/java/com/powsybl/ws/commons/computation/ComputationUtilTest.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.ws.commons.computation;
 
 import com.powsybl.iidm.network.*;
@@ -16,6 +22,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+/**
+ * @author Jamal KHEYYAD <jamal.kheyyad at rte-international.com>
+ */
 class ComputationUtilTest {
 
     static Network createBusBreakerNetwork() {

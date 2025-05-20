6cbd4d17f85f6506b1546a7956bfbedd6a5967b3
Thang PHAM <phamthang37@gmail.com>
2025-05-20 16:13:01 +0200

    Add unit test for ZipUtils
    Signed-off-by: Thang PHAM <phamthang37@gmail.com>

--- src/main/java/com/powsybl/ws/commons/ZipUtils.java
+++ src/main/java/com/powsybl/ws/commons/ZipUtils.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 package com.powsybl.ws.commons;
 
 import java.io.IOException;
@@ -8,6 +15,9 @@ import java.util.stream.Stream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
+/**
+ * @author Thang PHAM <quyet-thang.pham at rte-france.com>
+ */
 public final class ZipUtils {
     private ZipUtils() {
         throw new AssertionError("Utility class should not be instantiated");

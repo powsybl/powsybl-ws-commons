a1b5858866486b8e3b139bcf19f88373e378cbad
Rehili Ghazwa <ghazwarhili@gmail.com>
2025-07-09 11:09:19 +0200

    add copyrights and author
    Signed-off-by: Rehili Ghazwa <ghazwarhili@gmail.com>

--- src/main/java/com/powsybl/ws/commons/computation/service/FilterService.java
+++ src/main/java/com/powsybl/ws/commons/computation/service/FilterService.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.ws.commons.computation.service;
 
 import com.powsybl.ws.commons.computation.dto.GlobalFilter;
@@ -7,6 +13,9 @@ import org.gridsuite.filter.FilterLoader;
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * @author Rehili Ghazwa <ghazwa.rehili at rte-france.com>
+ */
 public interface FilterService extends FilterLoader {
     List<ResourceFilterDTO> getResourceFilters(UUID networkUuid, String variantId, GlobalFilter globalFilter);
 }

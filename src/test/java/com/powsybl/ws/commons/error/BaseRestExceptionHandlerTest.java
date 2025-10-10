661574285a7b23fc6e09b6039ed05a55f505fc84
benrejebmoh <mohamed.ben-rejeb@rte-france.com>
2025-10-10 12:17:53 +0200

    add license and author headers
    Signed-off-by: benrejebmoh <mohamed.ben-rejeb@rte-france.com>

--- src/test/java/com/powsybl/ws/commons/error/BaseRestExceptionHandlerTest.java
+++ src/test/java/com/powsybl/ws/commons/error/BaseRestExceptionHandlerTest.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.ws.commons.error;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -16,6 +22,9 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ * @author Mohamed Ben-rejeb {@literal <mohamed.ben-rejeb at rte-france.com>}
+ */
 class BaseRestExceptionHandlerTest {
 
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper().registerModule(new JavaTimeModule());
